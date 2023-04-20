ecf26ef6a033ace1abb532578b252ad17cc29a55
Ozir1z <robbertdewilde@gmail.com>
2023-04-20 11:56:30 -0600

    add random movement to llama

--- Farm/Farm.cs
+++ Farm/Farm.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class Farm : Node
 {
-    private VoidCharacter _player;
+  //  private VoidCharacter _player;
 
     public override void _Ready()
     {
         base._Ready();
 
-        _player = GetNode<VoidCharacter>("Void");
+      //  _player = GetNode<VoidCharacter>("Void");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
-        GetTree().CallGroup("AI", "UpdateAITargetLocation", _player.GlobalPosition);
+        //GetTree().CallGroup("AI", "UpdateAITargetLocation", _player.GlobalPosition);
     }
 }
--- Llama/llamaCharacter.cs
+++ Llama/llamaCharacter.cs
@@ -1,30 +1,76 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class LlamaCharacter : BaseCharacter
 {
+    public enum LlamaStatus { Idle, IdleClean, IdlePee, Walk };
+    public LlamaStatus CurrentStatus = LlamaStatus.Idle;
+
     protected const float Speed = 2f;
+
     private AnimationPlayer animPlayer;
     private NavigationAgent3D navAgent;
+    private LlamaStatus[] _idleStatusses = { LlamaStatus.IdlePee, LlamaStatus.IdleClean, LlamaStatus.Idle };
+
+    private int _roamRadius = 5;
+    private float _locationTolerance = 0.5f;
+    private Vector3 _nextLocation; // if this doesnt get used outside of process, make local variables
+    private Vector3 _startLocation;
+    private bool _isMoving = false;
+    private double _idleTimer = 0;
+
     public override void _Ready()
     {
         animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
 
         animPlayer.Play("Idle");
+
+        _nextLocation = GlobalPosition;
+        _startLocation = GlobalPosition;
     }
 
+    public override void _Process(double delta)
+    {
+        if ((GlobalPosition - _nextLocation).Length() < _locationTolerance && CurrentStatus == LlamaStatus.Walk)
+        {
+            Random rand = new();
+            CurrentStatus = _idleStatusses[rand.Next(0, _idleStatusses.Length)];
+            _idleTimer = 0;
+        }
+ 
+        // currently in an idle status
+        if (_idleStatusses.Contains(CurrentStatus))
+        {
+            if (CurrentStatus != LlamaStatus.Idle && animPlayer.CurrentAnimationPosition >= animPlayer.CurrentAnimationLength)
+            {
+                Random rand = new();
+                CurrentStatus = _idleStatusses[rand.Next(0, _idleStatusses.Length)];
+            }
 
-    // TODO add random pathing on navmesh and have random indle animations playing
+           if(CurrentStatus == LlamaStatus.Idle)
+                _idleTimer += delta;
+        }
+
+
+        // make amoutn idle time random
+        if (_idleTimer > 3 && CurrentStatus == LlamaStatus.Idle)
+        {
+            Random random = new();
+            int randomX = random.Next(-_roamRadius, _roamRadius);
+            int randomZ = random.Next(-_roamRadius, _roamRadius);
+            _nextLocation = _startLocation - new Vector3(randomX, 0, randomZ);
+            navAgent.TargetPosition = _nextLocation;
+            CurrentStatus = LlamaStatus.Walk;
+        }
+    }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
-        if (Velocity == Vector3.Zero)
-            animPlayer.Play("Idle");
-
-        if (IsOnFloor())
+        if (IsOnFloor() && CurrentStatus == LlamaStatus.Walk)
         {
             Vector3 currentLocation = GlobalPosition;
             Vector3 nextLocation = navAgent.TargetPosition;
@@ -32,16 +78,31 @@ public partial class LlamaCharacter : BaseCharacter
 
             LookAt(new Vector3(nextLocation.X, currentLocation.Y, nextLocation.Z));
             Velocity = Velocity.MoveToward(newVelocity, .25f);
-   
-            if (Velocity != Vector3.Zero)
-                animPlayer.Play("Walk");
 
-            MoveAndSlide();
         }
+        if(_idleStatusses.Contains(CurrentStatus))
+            Velocity = Vector3.Zero;
+
+        MoveAndSlide();
+        PlayAnimation();
     }
 
-    public void UpdateAITargetLocation(Vector3 targetLocation)
+    private void PlayAnimation()
     {
-        navAgent.TargetPosition = targetLocation;
+        switch (CurrentStatus)
+        {
+            case LlamaStatus.Walk:
+                animPlayer.Play("Walk");
+                break;
+            case LlamaStatus.Idle:
+                animPlayer.Play("Idle");
+                break;
+            case LlamaStatus.IdlePee:
+                animPlayer.Play("Idle_Pee");
+                break;
+            case LlamaStatus.IdleClean:
+                animPlayer.Play("Idle_Clean");
+                break;
+        }
     }
 }
