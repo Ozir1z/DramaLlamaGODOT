177960396f8e954649e098bd0cf89b5359fb89fc
Ozir1z <robbertdewilde@gmail.com>
2023-04-27 23:23:23 -0600

    clean up animation for llama and farmer

--- Farmer/FarmerCharacter.cs
+++ Farmer/FarmerCharacter.cs
@@ -1,15 +1,23 @@
 using Godot;
 using System;
+using System.Linq;
+using static LlamaCharacter;
 
 public partial class FarmerCharacter : BaseCharacterDramaLlama
 {
+    public enum FarmerStatus { Idle, Walk, Talk };
+
+    protected FarmerStatus _currentStatus = FarmerStatus.Idle;
     protected const float Speed = 2f;
 
     private AnimationPlayer _animPlayer;
     private NavigationAgent3D _navAgent;
     private Vector3 _moveToPoint;
+    private string[] _idleAnimations = {"Idle_Scratch", "Idle_Inspect"};
+    private string _nextIdleAnimation = "Idle";
+    private const double _idleTimerTime = 2;
+    private double _idleTimer = _idleTimerTime;
 
-    private bool _movingToPoint = false;
     public override void _Ready()
     {
         _animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
@@ -20,29 +28,61 @@ public partial class FarmerCharacter : BaseCharacterDramaLlama
 
     public override void _Input(InputEvent @event)
     {
+        // input for the teaser
         if(@event.IsActionPressed("moveFarmer"))
         {
-            _movingToPoint = true;
+            _currentStatus = FarmerStatus.Walk;
             _navAgent.TargetPosition = _moveToPoint;
         }
         if(@event.IsActionPressed("talkFarmer"))
         {
-
+            if(_currentStatus == FarmerStatus.Idle)
+                _currentStatus = FarmerStatus.Talk;
+            else if(_currentStatus == FarmerStatus.Talk)
+            {
+                SwitchToIdleStatus();
+            }
         }
     }
 
     public override void _Process(double delta)
     {
-        if (_navAgent.DistanceToTarget() < 0.5f)
-            _movingToPoint = false;
+        if (_navAgent.DistanceToTarget() < 0.5f && _currentStatus == FarmerStatus.Walk)
+            SwitchToIdleStatus();
 
+        if (_currentStatus == FarmerStatus.Idle)
+        {
+            _idleTimer -= delta;
+
+            if (_nextIdleAnimation == "Idle" && _idleTimer <= 0)
+            {
+                SwitchToIdleStatus();
+                Random rand = new();
+                int chance = rand.Next(1, 5);// 25% chance
+
+                if (chance == 1)
+                {
+                    Random rand2 = new();
+                    int idleAnimationNr = rand2.Next(0, _idleAnimations.Length);
+                    _nextIdleAnimation = _idleAnimations[idleAnimationNr];
+
+                }
+            }
+           
+            if (_idleAnimations.Contains(_animPlayer.CurrentAnimation)
+                && _animPlayer.CurrentAnimationPosition + 0.1 >= _animPlayer.CurrentAnimationLength)
+            {
+                _nextIdleAnimation = "Idle";
+                SwitchToIdleStatus();
+            }
+        }
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
-        if(_movingToPoint)
+        if(_currentStatus == FarmerStatus.Walk)
         {
             Vector3 currentLocation = GlobalPosition;
             Vector3 nextLocation = _navAgent.GetNextPathPosition();
@@ -52,16 +92,36 @@ public partial class FarmerCharacter : BaseCharacterDramaLlama
             LookAt(lookLocation);
 
             Velocity = Velocity.MoveToward(newVelocity, .25f);
-
-            _animPlayer.Play("Walk");
         }
-        else
-        {
+        else if(_currentStatus == FarmerStatus.Idle)
             Velocity = new Vector3(Vector3.Zero.X, Velocity.Y, Vector3.Zero.Z);
-            _animPlayer.Play("Idle");
-        }
-           
+
+
 
         MoveAndSlide();
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        switch (_currentStatus)
+        {
+            case FarmerStatus.Walk:
+                _animPlayer.Play("Walk", .2);
+                break;
+            case FarmerStatus.Idle:
+                _animPlayer.Play(_nextIdleAnimation, .2);
+                break;
+            case FarmerStatus.Talk:
+                _animPlayer.Play("Talk", .2);
+                break;
+        }
+    }
+
+
+    private void SwitchToIdleStatus()
+    {
+        _currentStatus = FarmerStatus.Idle;
+        _idleTimer = _idleTimerTime;
     }
 }
--- Llama/llamaCharacter.cs
+++ Llama/llamaCharacter.cs
@@ -1,22 +1,26 @@
 using Godot;
 using System;
 using System.Linq;
+using static FarmerCharacter;
 
 public partial class LlamaCharacter : BaseCharacterDramaLlama
 {
-    public enum LlamaStatus { Idle, IdleClean, IdlePee, Walk };
+    public enum LlamaStatus { Idle, Walk };
     public LlamaStatus CurrentStatus = LlamaStatus.Idle;
 
     protected const float Speed = 2f;
 
     private AnimationPlayer _animPlayer;
     private NavigationAgent3D _navAgent;
-    private readonly LlamaStatus[] _idleStatusses = { LlamaStatus.IdlePee, LlamaStatus.IdleClean, LlamaStatus.Idle };
+    private string[] _idleAnimations = { "Idle_Pee", "Idle_Clean", "Idle_Eating" };
+    private string _nextIdleAnimation = "Idle";
+    private const double _SpecialIdleTimerTime = 2;
+    private double _specialIdleTimer = _SpecialIdleTimerTime;
+    private double _idleTimer = 0;
 
     private int _roamRadius = 5;
     private float _locationTolerance = 0.5f;
     private Vector3 _startLocation;
-    private double _idleTimer = 0;
 
     public override void _Ready()
     {
@@ -32,28 +36,40 @@ public partial class LlamaCharacter : BaseCharacterDramaLlama
     {
         if (_navAgent.DistanceToTarget() < _locationTolerance && CurrentStatus == LlamaStatus.Walk)
         {
+            SwitchToIdleStatus();
             Random rand = new();
-            CurrentStatus = _idleStatusses[rand.Next(0, _idleStatusses.Length)];
-            _idleTimer = 0;
+            _idleTimer = rand.Next(3, 6);
         }
- 
-        // currently in an idle status
-        // instead of a random idle status, give 20% chance to trigger a special idle witihin the idle (but only once)
-        if (_idleStatusses.Contains(CurrentStatus))
+
+        if (CurrentStatus == LlamaStatus.Idle)
         {
-            if (CurrentStatus != LlamaStatus.Idle && _animPlayer.CurrentAnimationPosition >= _animPlayer.CurrentAnimationLength)
+            _specialIdleTimer -= delta;
+            if(_animPlayer.CurrentAnimation.Equals("Idle"))
+                _idleTimer -= delta;
+
+            if (_nextIdleAnimation == "Idle" && _specialIdleTimer <= 0)
             {
+                SwitchToIdleStatus();
                 Random rand = new();
-                CurrentStatus = _idleStatusses[rand.Next(0, _idleStatusses.Length)];
+                int chance = rand.Next(1, 5);// 25% chance
+
+                if (chance == 1)
+                {
+                    Random rand2 = new();
+                    int idleAnimationNr = rand2.Next(0, _idleAnimations.Length);
+                    _nextIdleAnimation = _idleAnimations[idleAnimationNr];
+
+                }
             }
 
-           if(CurrentStatus == LlamaStatus.Idle)
-                _idleTimer += delta;
+            if (_idleAnimations.Contains(_animPlayer.CurrentAnimation)
+                && _animPlayer.CurrentAnimationPosition + 0.1 >= _animPlayer.CurrentAnimationLength)
+            {
+                _nextIdleAnimation = "Idle";
+                SwitchToIdleStatus();
+            }
         }
-
-
-        // make amount idle time random
-        if (_idleTimer > 3 && CurrentStatus == LlamaStatus.Idle)
+        if (_idleTimer <= 0 && CurrentStatus == LlamaStatus.Idle)
         {
             Random random = new();
             int randomX = random.Next(-_roamRadius, _roamRadius);
@@ -76,9 +92,8 @@ public partial class LlamaCharacter : BaseCharacterDramaLlama
             Vector3 lookLocation = new(nextLocation.X, currentLocation.Y, nextLocation.Z);
             LookAt(lookLocation);
             Velocity = Velocity.MoveToward(newVelocity, .25f);
-
         }
-        if(_idleStatusses.Contains(CurrentStatus))
+        else if (CurrentStatus == LlamaStatus.Idle)
             Velocity = new Vector3(Vector3.Zero.X, Velocity.Y, Vector3.Zero.Z);
 
         MoveAndSlide();
@@ -90,17 +105,18 @@ public partial class LlamaCharacter : BaseCharacterDramaLlama
         switch (CurrentStatus)
         {
             case LlamaStatus.Walk:
-                _animPlayer.Play("Walk");
+                _animPlayer.Play("Walk", .2);
                 break;
             case LlamaStatus.Idle:
-                _animPlayer.Play("Idle");
-                break;
-            case LlamaStatus.IdlePee:
-                _animPlayer.Play("Idle_Pee");
-                break;
-            case LlamaStatus.IdleClean:
-                _animPlayer.Play("Idle_Clean");
+                _animPlayer.Play(_nextIdleAnimation, .2);
                 break;
         }
+    }
+
+
+    private void SwitchToIdleStatus()
+    {
+        CurrentStatus = LlamaStatus.Idle;
+        _specialIdleTimer = _SpecialIdleTimerTime;
     }
 }
